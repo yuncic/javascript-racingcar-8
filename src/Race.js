d7a1122e3cdfa4d9816605b7d3f8e474f579cb6e
김상윤 <ksy151020@gmail.com>
2025-10-23 22:14:14 +0900

    feat: Race.js 구현

---
+++ src/Race.js
@@ -0,0 +1,24 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import { RANDOM } from "./Constants.js";
+
+class Race {
+    constructor(cars) {
+        this.cars = cars;
+    }
+    playRound() {
+    this.cars.forEach ((car) => {
+        const random = MissionUtils.Random.picNumberInRange(RANDOM.MIN,RANDOM.MAX);
+        if (random >= RANDOM.MOVE_CONDITION) {
+            car.move();
+        }
+    });
+}
+    getWinners() {
+        const MAX_POISTION = Math.max(...this.cars.map((car) => car.postion));
+        return this.cars
+            .filter((car) => car.position === MAX_POISTION)
+            .map((car) => car.name);
+  }
+}
+
+
