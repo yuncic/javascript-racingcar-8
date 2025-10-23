2d5f9a81b05efe451f4a19958c4f02c66d475722
김상윤 <ksy151020@gmail.com>
2025-10-23 21:06:56 +0900

    feat: Constants.js 생성

---
+++ src/Constants.js
@@ -0,0 +1,17 @@
+export const MESSAGES = {
+    INPUT_CAR_NAMES: "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분",
+    INPUT_TRY_COUNT: "시도할 횟수는 몇 회인가요?",
+    RESULT: "\n실행 결과",
+    WINNER: "최종 우승자 : "
+};
+
+export const ERROR_MESSAGES = {
+    INVALID_NAME: "[ERROR] 자동차 이름은 1글자 이상 5글자 이하만 가능합니다",
+    INVALID_TRY_COUNT: "[ERROR] 시도 횟수는 양의 정수여야 합니다"
+};
+
+export const RANDOM = {
+    MIN: 0,
+    MAX: 9,
+    MOVE_CONDITION: 4
+};
